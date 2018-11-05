091dc8c02ce2eb747831854702587a412cb909ca
= <=>
2018-11-05 18:17:23 +0200

    initialize files

---
+++ TamerQasaly/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Helpers;
+using DevExpress.XtraScheduler;
+using TamerQasaly.MainOffice;
+
+namespace TamerQasaly
+{
+    public partial class Form1 : RibbonForm
+    {
+        frmOffice_MainCareerType frm;
+        public Form1()
+        {
+            InitializeComponent();
+            InitSkinGallery();
+            
+        }
+        void InitSkinGallery()
+        {
+            SkinHelper.InitSkinGallery(rgbiSkins, true);
+        }
+
+        private void btnOffice_CareerType_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            frm = new frmOffice_MainCareerType();
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ TamerQasaly/MainOffice/frmOffice_MainCareerTypeAddEdit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using TamerQasaly.DBModel;
+
+namespace TamerQasaly.MainOffice
+{
+    public partial class frmOffice_MainCareerTypeAddEdit : DevExpress.XtraEditors.XtraForm
+    {
+        public static int intCareerTypeID = 0 ;
+        private TamerQasalyDBEntities db ;
+        private Office_CareerType oc;
+
+        public frmOffice_MainCareerTypeAddEdit()
+        {
+            InitializeComponent();
+
+            db = new TamerQasalyDBEntities();
+
+            
+        }
+
+        private void frmOffice_MainCareerTypeAddEdit_Load(object sender, EventArgs e) {
+            FillData(intCareerTypeID);
+        }   
+        
+        private void FillData(int id)
+        {
+            if (id != 0)
+            {
+                oc = db.Office_CareerType.Where(x => x.ID == intCareerTypeID).Single();
+                txtName.Text = oc.Name.ToString();
+                txtDescription.Text = oc.Description.ToString();
+            }
+            else
+            {
+                return;
+            }
+        }        
+    }
+}
